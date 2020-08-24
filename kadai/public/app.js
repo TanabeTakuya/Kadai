db93c965aaa2c4ec77d8aa05302cb632f7d84a54
TanabeTakuya <soranari-kuuya@ezweb.ne.jp>
2020-08-24 15:24:10 +0900

    ゲームの機能を作成

--- kadai/public/app.js
+++ kadai/public/app.js
@@ -9,11 +9,15 @@ const IAM = {
   //-------------------------------------
   const socket = io();
   
+  let myRoll;
+  let win = 2;
+  let an;
   // 正常に接続したら
   socket.on("connect", ()=>{
     // 表示を切り替える
     $("#my-roll").style.display = "none";   // 「接続中」を非表示
     $("#nowconnecting").style.display = "none";   // 「接続中」を非表示
+    $("#return").style.display = "none";   // 「接続中」を非表示
     $("#inputmyname").style.display = "block";    // 名前入力を表示
   });
   
@@ -42,9 +46,22 @@ const IAM = {
     IAM.name = myname.value;
     $("#question").addEventListener("click", (e)=>{
       // 表示を切り替える
+      document.getElementById("my-roll").innerText = "出題者"
       $("#my-roll").style.display = "block";   // 「接続中」を非表示
       $("#inputmyname").style.display = "none";   // 名前入力を非表示
       $("#chat").style.display = "block";         // チャットを表示
+
+      myRoll = "Question";
+    });
+    $("#ansewer").addEventListener("click", (e)=>{
+      // 表示を切り替える
+      document.getElementById("my-roll").innerText = "回答者"
+      document.getElementById("ansewer-question").innerText = "出題があるまでお待ちください"      
+      $("#my-roll").style.display = "block";   // 「接続中」を非表示
+      $("#inputmyname").style.display = "none";   // 名前入力を非表示
+      $("#chat").style.display = "block";         // チャットを表示
+
+      myRoll = "Ansewer";
     });
 
   });
@@ -59,19 +76,33 @@ const IAM = {
   $("#frm-post").addEventListener("submit", (e)=>{
     // 規定の送信処理をキャンセル(画面遷移しないなど)
     e.preventDefault();
-  
+    let operation = document.getElementById("ansewer-question");
     // 入力内容を取得する
     const msg = $("#msg");
-    if( msg.value === "" ){
+    if( msg.value === "" || operation.value === "回答があるまでお待ちください"){
       return(false);
     }
-  
-    // Socket.ioサーバへ送信
-    socket.emit("post", {
-      text: msg.value,
-      token: IAM.token,
-      name: IAM.name
-    });
+
+    if(myRoll ==="Question"){
+      // Socket.ioサーバへ送信
+      socket.emit("post", {
+        text: msg.value,
+        token: IAM.token,
+        name: IAM.name
+      });
+      document.getElementById("ansewer-question").innerText = "回答があるまでお待ちください" ;     
+    }
+    else if(operation != "出題があるまでお待ちください"){
+      // Socket.ioサーバへ送信
+      socket.emit("post", {
+        text: msg.value,
+        token: IAM.token,
+        name: IAM.name
+      });
+      if(operation === text){
+        an = msg.value;
+      }
+    }
   
     // 発言フォームを空にする
     msg.value = "";
@@ -96,20 +127,45 @@ const IAM = {
   function addMessage(msg, is_me=false){
     const list = $("#msglist");
     const li = document.createElement("li");
-  
     //------------------------
     // 自分の発言
     //------------------------
     if( is_me ){
-      li.innerHTML = `<span class="msg-me">${msg.name}> ${msg.text}</span>`;
+      if(myRoll ==="Ansewer"){
+        li.innerHTML = `<span class="msg-me">${msg.name}> ${msg.text}</span>`;
+        if(an === msg.text){
+          win = 1;
+          document.getElementById("ansewer-question").innerText = "正解";
+          $("#return").style.display = "none"; 
+        }
+        else{
+          document.getElementById("ansewer-question").innerText = "不正解";
+          win = 0;
+          $("#return").style.display = "none";
+        }
+      }
+      else if(document.getElementById("ansewer-question").innerText != "回答があるまでお待ちください" ){
+      }
     }
-    //------------------------
-    // 自分以外の発言
-    //------------------------
-    else{
-      li.innerHTML =  `<span class="msg-not-me">${msg.name}> ${msg.text}</span>`;
+    else if(myRoll ==="Ansewer"){
+      an = msg.text;
+      let a = an.split('');
+      let questionRand = a.sort();
+      document.getElementById("ansewer-question").innerText = questionRand.join("");
     }
-  
+    else if(myRoll ==="Question" ){
+      if(win === 0){
+        document.getElementById("ansewer-question").innerText = "貴方の勝ち";
+        $("#return").style.display = "none";
+      }
+      else{
+        document.getElementById("ansewer-question").innerText = "貴方の負け";
+        $("#return").style.display = "none"; 
+      }
+    }
+    document.getElementById("return").onclick = function() {
+      //最初に戻りたい
+    };
     // リストの最初に追加
     list.insertBefore(li, list.firstChild);
   }
